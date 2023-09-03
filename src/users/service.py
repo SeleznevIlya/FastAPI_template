8a2bf94f7888173d2da2fd8519a1cccfdff3d513
Ilya Seleznev <seleznev.ilya.a@gmail.com>
2023-09-03 22:15:09 +0300

    add UserService

--- src/users/service.py
+++ src/users/service.py
@@ -2,9 +2,10 @@ import uuid
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import jwt
+from fastapi import HTTPException, status
 
-from .utils import is_valid_password
-from .schemas import Token
+from .utils import is_valid_password, get_password_hash
+from .schemas import Token, UserCreate, UserCreateDB, UserUpdate, UserUpdateDB, User
 from ..config import settings
 from ..database import async_session_maker
 from .repository import RefreshSessionRepository, UserRepository
@@ -104,4 +105,123 @@ class AuthService:
 
 
 class UserService:
-    pass
+
+    @classmethod
+    async def create_new_user(cls, user: UserCreate) -> UserModel:
+        async with async_session_maker() as session:
+            user_exist = await UserRepository.find_one_or_none(session, email=user.email)
+            if user_exist:
+                raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT, detail="User already exists"
+                )
+            
+            # user.is_superuser = False
+            # user.is_verified = False
+            db_user = await UserRepository.add(
+                session,
+                UserCreateDB(
+                    **user.model_dump,
+                    hashed_password=get_password_hash(user.password)
+                )
+            )
+            await session.commit()
+        return db_user
+
+    @classmethod
+    async def get_user(cls, user_id: uuid.UUID) -> UserModel:
+        async with async_session_maker() as session:
+            db_user = await UserRepository.find_one_or_none(session, id=user_id)
+        if db_user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            )
+        return db_user
+
+    @classmethod
+    async def update_user(cls, user_id: uuid.UUID, user: UserUpdate) -> UserModel:
+        async with async_session_maker() as session:
+            db_user = await UserRepository.find_one_or_none(session, id=user_id)
+            if db_user is None:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+                )
+            
+            if user.password:
+                user_in = UserUpdateDB(
+                    **user.model_dump(
+                        exclude=("is_active", "is_verified", "is_superuser"),
+                        exclude_unset=True
+                    ),
+                    hashed_password=get_password_hash(user.password)
+                )
+            else:
+                user_in = UserUpdateDB(**user.model_dump())
+
+            user_update = await UserRepository.update(
+                session,
+                UserModel.id == user_id,
+                obj_in=user_in
+            )
+            await session.commit()
+        return user_update
+
+    @classmethod
+    async def delete_user(cls, user_id: uuid.UUID) -> None:
+        async with async_session_maker() as session:
+            db_user = await UserRepository.find_one_or_none(session, id=user_id)
+            if db_user is None:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+                )
+            await UserRepository.update(
+                session,
+                UserModel.id == user_id,
+                {"is_active": False}
+            )
+            await session.commit()
+
+    @classmethod
+    async def get_user_list(cls, *filter, offset: int = 0, limit: int = 100, **filter_by) -> List[UserModel]:
+        async with async_session_maker() as session:
+            users = await UserRepository.find_all(session, *filter, offset=offset, limit=limit, **filter_by)
+        if users is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Users not found"
+            )
+        return users
+
+        # return [
+        #     User(
+        #         id=str(db_user.id),
+        #         email=db_user.email,
+        #         fio=db_user.fio,
+        #         is_active=db_user.is_active,
+        #         is_superuser=db_user.is_superuser
+        #     ) for db_user in users
+        # ]
+        
+        
+        
+    @classmethod
+    async def update_user_from_superuser(cls, user_id: uuid.UUID, user: UserUpdate) -> User:
+        async with async_session_maker() as session:
+            db_user = await UserRepository.find_one_or_none(session, id=user_id)
+            if db_user is None:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+                )
+            
+            user_in = UserUpdateDB(**user.model_dump(exclude_unset=True))
+            user_update = await UserRepository.update(
+                session,
+                UserModel.id == user_id,
+                obj_in=user_id
+            )
+            await session.commit()
+            return user_update
+
+    @classmethod
+    async def delete_user_from_superuser(cls, user_id: uuid.UUID) -> None:
+        async with async_session_maker() as session:
+            await UserRepository.delete(session, UserModel.id == user_id)
+            await session.commit()
